fa63e17272d129d7c52cca712f30628c02bcd4ba
= <=>
2026-04-08 18:20:27 -0600

    Finalizacion de practica

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,37 @@ console.log("Su pantalla posse: " + width + " pixeles de ancho y " + height+ " p
 console.log("window.screen.width: " + window.screen.width + " px "+ "window.screen.height: " + window.screen.height + " px")
 console.log("window.screen.availWidth: " + window.screen.availWidth + " px "+ "window.screen.availHeight: " + window.screen.availHeight + " px")
 console.log("window.body.clientWidth: " + document.body.clientWidth + " px "+ "window.body.clientHeight: " + document.body.clientHeight + " px")
+
+function calcular(n1, n2, salida){
+    primerSumando = document.getElementById(n1);
+    segundoSumando = document.getElementById(n2);
+
+    let calculo = (parseFloat(primerSumando.value) + parseFloat(segundoSumando.value));
+
+    document.getElementById(salida).innerHTML = calculo;
+}
+
+function crearElemento(url = null){
+    if(url !== null)
+    {
+        url = url;
+    }
+    else{
+        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_F_109008395_OQDupHMza1V6CNOzrJwWAKlaktT4IsEW.jpg";
+    }
+
+    let objetoimagen = document.createElement("img");
+
+    objetoimagen.id = "imagencreada"
+    objetoimagen.src = url;
+    objetoimagen.title = "Control de videojuego";
+    objetoimagen.width = 220;
+    objetoimagen.height = 165;
+    objetoimagen.addEventListener('click', function(){
+        objetoimagen.src = "https://encrypted-tbn0.gstatic.com/imagenes?q=tbn:ANd9GcTkU5595AC-bbsoPF-Ik0WzUbVj9rnAbs9lkw&s";
+        objetoimagen.width = 225;
+        objetoimagen.height = 225;
+    });
+
+    document.getElementById('lienzo').appendChild(objetoimagen);
+}
